1d0d968e23c49637a312a9d4eaa26c4af7b74a81
Tyler Nichols <nicholsty72@gmail.com>
2023-05-27 14:13:57 -0500

    initial styling complete, TODO - JS to give budget functionality

--- app/components/budget.tsx
+++ app/components/budget.tsx
@@ -2,21 +2,42 @@ import Image from "next/image";
 import data from "../api/data.json";
 
 export default function Budget() {
-  const total = 0;
+  interface ExpenseType {
+    day: string;
+    amount: number;
+  }
+  const balanceThisMonth = 0;
+  const totalThisMonth = 0;
+  const percentDifference = 0;
+
+  data.forEach(function (combine) {
+    console.log(data[0].amount);
+  });
 
   return (
-    <div className="flex">
-      <div className="soft-red flex justify-end p-4 my-2 rounded-xl">
+    <div className="flex flex-col">
+      <div className="bg-soft-red flex justify-between p-4 my-2 rounded-xl">
         <div className="flex flex-col">
           <h1 className="text-xl">My balance</h1>
-          <h2 className="text-5xl">${total}</h2>
+          <h2 className="text-5xl">${balanceThisMonth}</h2>
         </div>
-        <Image src="/logo.svg" height={50} width={50} alt="Logo" />
+        <Image src="/logo.svg" height={65} width={65} alt="Logo" />
       </div>
-      <div className="cream p-4 rounded-xl">
-        <h2 className="text-5xl dark-brown">
-          Spending - Last 7 days
-        </h2>
+      <div className="bg-cream p-4 rounded-xl">
+        <h3 className="text-5xl text-dark-brown">Spending - Last 7 days</h3>
+        <div className="w-full h-32">
+          <p className="text-5xl text-center">IMAGE</p>
+        </div>
+        <div className="border-t-2 border-medium-brown flex justify-between pt-4">
+          <div className="flex flex-col justify-center">
+            <p className="text-medium-brown text-xl">Total this month</p>
+            <h4 className="text-5xl text-dark-brown">{totalThisMonth}</h4>
+          </div>
+          <div className="flex flex-col content-end justify-center">
+            <h5 className="text-2xl text-dark-brown">{percentDifference}</h5>
+            <p className="text-medium-brown text-xl">from last month</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Budget from './components/budget';
 
 export default function Home() {
   return (
-    <main className="bg-orange-200 p-8">
+    <main className="h-screen bg-orange-200 p-8">
       <Budget/>
     </main>
   )
