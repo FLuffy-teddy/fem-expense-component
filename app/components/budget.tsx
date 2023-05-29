360946366cec3e80dcf6b65263abebe82c7ae223
Tyler Nichols <nicholsty72@gmail.com>
2023-05-28 22:39:29 -0500

    final commit

--- app/components/budget.tsx
+++ app/components/budget.tsx
@@ -10,11 +10,16 @@ Chart.register(CategoryScale);
 
 export const options = {
   responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    }
+  }
 };
 
 export default function Budget() {
-  const balanceThisMonth = 0;
-  const percentDifference = 0;
+  const balancePreviousMonths = 693.54;
+  const percentDifference = 2.4;
   let sum = 0;
   let callBackIncrement = 0;
 
@@ -23,14 +28,13 @@ export default function Budget() {
     callBackIncrement++;
   });
 
+  const balanceThisMonth = balancePreviousMonths + sum;
+
   const dayLabel:string[] = data.map((list) => list.day);
   const amountPerDay:number[] = data.map((list) => list.amount);
 
   const barData = {
     labels: dayLabel,
-    legend: {
-      display: false
-    },
     datasets: [{
       data: amountPerDay,
       backgroundColor: [
@@ -57,26 +61,26 @@ export default function Budget() {
   
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col max-w-xl">
       <div className="bg-soft-red flex justify-between p-4 my-2 rounded-xl">
         <div className="flex flex-col">
           <h1 className="text-xl">My balance</h1>
-          <h2 className="text-5xl">${balanceThisMonth}</h2>
+          <h2 className="text-3xl md:text-5xl">${balanceThisMonth}</h2>
         </div>
         <Image src="/logo.svg" height={65} width={65} alt="Logo" />
       </div>
       <div className="bg-cream p-4 rounded-xl">
-        <h3 className="text-5xl text-dark-brown">Spending - Last 7 days</h3>
+        <h3 className="text-3xl md:text-5xl text-dark-brown">Spending - Last 7 days</h3>
         <div className="w-full">
         <Bar options={options} data={barData} />
         </div>
         <div className="border-t-2 border-medium-brown flex justify-between pt-4">
           <div className="flex flex-col justify-center">
             <p className="text-medium-brown text-xl">Total this month</p>
-            <h4 className="text-5xl text-dark-brown">${sum}</h4>
+            <h4 className="text-3xl md:text-5xl text-dark-brown">${sum}</h4>
           </div>
           <div className="flex flex-col content-end justify-center">
-            <h5 className="text-2xl text-dark-brown">{percentDifference}</h5>
+            <h5 className="text-2xl text-dark-brown text-right">{percentDifference}%</h5>
             <p className="text-medium-brown text-xl">from last month</p>
           </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Budget from './components/budget';
 
 export default function Home() {
   return (
-    <main className="bg-orange-200 p-4">
+    <main className="bg-orange-200 p-4 flex justify-center h-screen">
       <Budget/>
     </main>
   )
